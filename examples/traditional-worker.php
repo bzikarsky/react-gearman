00f0681849d71929bdf501dbcdaf21b294ccaefc
Benjamin Zikarsky <benjamin@zikarsky.de>
2013-12-17 13:26:49 +0100

    Added first examples
    Wrote a quick'n'dirty proof-of-concept client (examples/async-client.php)
    and took the example worker-implementation from php.net to provide a
    working demo.
    
    Also mentioned the examples in README.md and excluded them from PHPUnit.

---
+++ examples/traditional-worker.php
@@ -0,0 +1,47 @@
+<?php
+
+// Taken from http://www.php.net/manual/en/gearmanworker.wait.php example#1
+
+echo "Starting\n";
+
+# Create our worker object
+$worker= new GearmanWorker();
+
+# Make the worker non-blocking
+$worker->addOptions(GEARMAN_WORKER_NON_BLOCKING);
+
+# Add the default server (localhost, port 4730)
+$worker->addServer("127.0.0.1", 4730);
+
+# Add our reverse function
+$worker->addFunction('reverse', 'reverse_fn');
+
+# Try to grab a job
+while (@$worker->work() ||
+       $worker->returnCode() == GEARMAN_IO_WAIT ||
+       $worker->returnCode() == GEARMAN_NO_JOBS)
+{
+  if ($worker->returnCode() == GEARMAN_SUCCESS)
+    continue;
+
+  echo "Waiting for next job...\n";
+  if (!@$worker->wait())
+  {
+    if ($worker->returnCode() == GEARMAN_NO_ACTIVE_FDS)
+    {
+      # We are not connected to any servers, so wait a bit before
+      # trying to reconnect.
+      sleep(5);
+      continue;
+    }
+    break;
+  }
+}
+
+echo "Worker Error: " . $worker->error() . "\n";
+
+function reverse_fn($job)
+{
+  return strrev($job->workload());
+}
+
